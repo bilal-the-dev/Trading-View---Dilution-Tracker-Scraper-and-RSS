e2a1348472a870d1bd641be80795117d00b815f2
bilal-the-dev <bilalfarooq6001@gmail.com>
2024-12-31 22:24:41 +0500

    add role restriction to analyzer

--- src/utils/interaction.js
+++ src/utils/interaction.js
@@ -24,6 +24,11 @@ exports.handleTickerAnalysisInteraction = async (
   try {
     await interaction.deferReply({ ephemeral });
 
+    const roles = process.env.ANALYZER_ACCESS_ROLE_IDS.split(",");
+
+    if (!roles.some((r) => interaction.member.roles.cache.has(r)))
+      throw new Error("You do not have perms to use the analyzer!");
+
     const description = await interaction.client.fetchInfoAboutTicker(ticker);
 
     // console.log(description);
