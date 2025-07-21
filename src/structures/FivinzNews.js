1466c9232e54650a3cde6c24b8333a2e818330e4
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-07-21 12:41:59 +0500

    update: change structure a bit

---
+++ src/structures/FivinzNews.js
@@ -0,0 +1,34 @@
+const cheerio = require("cheerio");
+
+const { FIVINZ_BASE_URL } = process.env;
+
+class FivinzNews {
+  static async fetchTickerNews(ticker) {
+    const url = `${FIVINZ_BASE_URL}/quote.ashx?t=${ticker}`;
+    const response = await fetch(url);
+
+    if (!response.ok) return "Some error occured";
+
+    const data = await response.json();
+
+    const $ = cheerio.load(data);
+
+    const firstRow = $("#news-table tr").first();
+
+    if (!firstRow.length) return;
+
+    const timestamp = firstRow.find("td").first().text().trim();
+
+    // Title & URL
+    const link = firstRow.find("a.tab-link-news");
+    const title = link.text().trim();
+    const href = link.attr("href");
+    const fullUrl = href ? `${FIVINZ_BASE_URL}${href}` : FIVINZ_BASE_URL;
+
+    const rightTextHtml = firstRow.find("div.news-link-right").text()?.trim();
+
+    return `[${title}}](${fullUrl})\n-# ${timestamp} ${rightTextHtml}`;
+  }
+}
+
+module.exports = FivinzNews;
